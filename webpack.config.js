7404b509fa6456d95617cd5d88631b9be860a916
Tony Watson <tony@alphero.com>
2019-09-05 17:26:56 +1200

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './app/index.js',
@@ -25,7 +26,12 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'app/index.html'
-    })
+    }),
+    new CopyPlugin([
+      {
+        from: '_redirects'
+      }
+    ])
   ],
   devServer: {
     historyApiFallback: true
